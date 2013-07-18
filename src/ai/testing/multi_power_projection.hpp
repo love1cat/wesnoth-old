751a6121789a63718deb527f6ec6cfb6d37a3208
love1cat <yuan.uconn@gmail.com>
2013-07-18 16:38:32 -0400

    Add in max weight matching files for AI improvement
    Implementation of hungary algorithm. Run in O(n^3), n is cardinality of
    bipartite graph

--- src/ai/testing/multi_power_projection.hpp
+++ src/ai/testing/multi_power_projection.hpp
@@ -1,14 +1,14 @@
 /*
  Copyright (C) 2009 - 2013 by Yuan Song <yuan.uconn@gmail.com>
  Part of the Battle for Wesnoth Project http://www.wesnoth.org/
- 
+
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY.
- 
+
  See the COPYING file for more details.
  */
 
@@ -23,11 +23,16 @@
 #include "target_analysis_strategy.hpp"
 
 namespace ai {
-	
+
 namespace testing_ai_default {
-	//class
-}
+
+class multi_power_projection_strategy : public target_analysis_strategy1 {
+	multi_power_projection_strategy(const config& cfg);
+	virtual double power_projection(const readonly_context* ai_ptr, const std::vector<map_location>& locs, const move_map& dstsrc) const;
+};
 	
-}
+} // end of namespace testing_ai_default
+	
+} // end of namespace ai
 
 #endif /* defined(AI_TESTING_MULTI_POWER_PROJECTION_HPP) */
